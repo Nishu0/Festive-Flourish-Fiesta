f2008dfd1a872fc8d32f68a6c20f720744565dfa
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-12-13 20:24:18 +0530

    :sparkles: Not Found Custom Christmas Page

---
+++ components/NotFound.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div id="container">
+        <div id="snowcape-container"></div>
+        <div id="snowflakes">
+            <div id="snowflake"></div>
+            <div id="snowflake"></div>
+            <div id="snowflake"></div>
+            <div id="snowflake"></div>
+            <div id="snowflake"></div>
+            <div id="snowflake"></div>
+            <div id="snowflake"></div>
+            <div id="snowflake"></div>
+            <div id="snowflake"></div>
+            <div id="snowflake"></div>
+        </div>
+        <div id="box">
+            <div id="santa"></div>
+            <p id="para">No gifts here!</p>
+        </div>
+        <div id="btn-home">
+            <a href="/">HOME</a>
+        </div>
+
+    </div>
+  )
+}
+
+export default NotFound
